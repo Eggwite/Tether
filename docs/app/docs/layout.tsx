9194f1810ea488e67a4e20bdd0b8ee46d832b5d9
Eggwite <69743130+Eggwite@users.noreply.github.com>
2025-12-18 17:49:20 +1300

    refactor: update layout and documentation structure; enhance GitHub info integration

--- docs/app/docs/layout.tsx
+++ docs/app/docs/layout.tsx
@@ -1,11 +1,23 @@
-import { source } from "@/lib/source";
-import { DocsLayout } from "fumadocs-ui/layouts/docs";
+import { DocsLayout, type DocsLayoutProps } from "fumadocs-ui/layouts/docs";
 import { baseOptions } from "@/lib/layout.shared";
+import { source } from "@/lib/source";
+import { GithubInfo } from "fumadocs-ui/components/github-info";
 
-export default function Layout({ children }: LayoutProps<"/docs">) {
-  return (
-    <DocsLayout tree={source.pageTree} {...baseOptions()}>
-      {children}
-    </DocsLayout>
-  );
+function docsOptions(): DocsLayoutProps {
+  return {
+    ...baseOptions(),
+    tree: source.pageTree,
+    links: [
+      {
+        type: "custom",
+        children: (
+          <GithubInfo owner="Eggwite" repo="Tether" className="lg:-mx-2" />
+        ),
+      },
+    ],
+  };
+}
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return <DocsLayout {...docsOptions()}>{children}</DocsLayout>;
 }
